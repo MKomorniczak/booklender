78c7852e3d394fe409a9ae452c0f002dd5885c4b
retaz <retazz1991@gmail.com>
2021-04-27 14:29:15 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/marek/booklender/Entity/LibraryUser.java
@@ -0,0 +1,77 @@
+package se.lexicon.marek.booklender.Entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class LibraryUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private int userId;
+
+    @Column(nullable = false)
+    private LocalDate regDate;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LibraryUser(LocalDate regDate, String name, String email) {
+        this.regDate = regDate;
+        this.name = name;
+        this.email = email;
+    }
+
+    public LibraryUser() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LibraryUser that = (LibraryUser) o;
+        return userId == that.userId && Objects.equals(regDate, that.regDate) && Objects.equals(name, that.name) && Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, regDate, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryUser{" +
+                "userId=" + userId +
+                ", regDate=" + regDate +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/se/lexicon/marek/booklender/Entity/Loan.java
@@ -0,0 +1,128 @@
+package se.lexicon.marek.booklender.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long loanId;
+    private LibraryUser loanTaker;
+    private Book book;
+    private LocalDate loanDate;
+    private boolean terminated;
+
+    public Loan(LibraryUser loanTaker, Book book, LocalDate loanDate, boolean terminated) {
+        this.loanTaker = loanTaker;
+        this.book = book;
+        this.loanDate = loanDate;
+        this.terminated = terminated;
+    }
+
+    public Loan() {
+    }
+
+    public long getLoanId() {
+        return loanId;
+    }
+
+    public LibraryUser getLoanTaker() {
+        return loanTaker;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public boolean isTerminated() {
+        return terminated;
+    }
+
+
+    public void setLoanTaker(LibraryUser loanTaker) {
+        this.loanTaker = loanTaker;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public void setTerminated(boolean terminated) {
+        this.terminated = terminated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Loan loan = (Loan) o;
+        return loanId == loan.loanId && terminated == loan.terminated && Objects.equals(loanTaker, loan.loanTaker) && Objects.equals(book, loan.book) && Objects.equals(loanDate, loan.loanDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(loanId, loanTaker, book, loanDate, terminated);
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "loanId=" + loanId +
+                ", loanTaker=" + loanTaker +
+                ", book=" + book +
+                ", loanDate=" + loanDate +
+                ", terminated=" + terminated +
+                '}';
+    }
+
+    public boolean isOverdue() {
+        // check if is terminated is true => false
+        // 2021-04-23 + 7 = 2021-04-30
+        // get loan date + maxloandays
+        // compare with current date
+
+if (isTerminated())
+    return false;
+else{
+    LocalDate overdueDate = loanDate.plusDays(book.getMaxLoanDays());
+    LocalDate now = LocalDate.now();
+    return now.isAfter(overdueDate);
+
+
+}
+/*        Book book = new Book();
+         book.setMaxLoanDays(7);
+        loanDate = LocalDate.parse("2021-04-20");
+        LocalDate overdueDate = loanDate.plusDays(book.getMaxLoanDays());
+        LocalDate now = LocalDate.now();
+        if (now.isAfter(overdueDate))
+            return true;
+        else {
+            return false;
+        }*/
+    }
+
+    public BigDecimal getFine() {
+
+        return null;
+    }
+
+
+    public boolean extendLoan(int days) {
+
+        return false;
+    }
+}
